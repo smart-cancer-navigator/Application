552bbf6f080937766dfca687d1ba78e98c2cabdb
Makiah Bennett <makiahtbennett@gmail.com>
2017-08-02 22:58:26 -0400

    Switch tabs to variant IDs and accordion to variant properties

--- src/app/visualize-results/clinical-trials/clinical-trials.component.ts
+++ src/app/visualize-results/clinical-trials/clinical-trials.component.ts
@@ -3,60 +3,52 @@
  * often is the best way to glean such information.
  */
 
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { ClinicalTrialsService } from './clinical-trials.service';
 import { ClinicalTrialReference } from './clinical-trials';
 import { Variant } from '../../global/genomic-data';
-import { USER_SELECTED_VARIANTS } from '../../data-entry/data-entry-form.component';
 
 @Component({
   selector: 'clinical-trials',
   template: `
     <!-- Thanks to the ng-bootstrap project :) -->
-    <ngb-accordion #acc="ngbAccordion">
-      <ngb-panel *ngFor="let variant of variants; let i = index;" title="{{variant.toIntelligentDisplayRepresentation()}}">
-        <ng-template ngbPanelContent>
-          <table class="table table-hover table-bordered">
-            <thead>
-            <tr>
-              <th>Clinical Trial ID</th>
-              <th>Phase</th>
-              <th>Brief Title</th>
-              <th>Drugs</th>
-              <th>Principal Investigator</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr *ngFor="let clinicalTrial of clinicalTrials[i]" class="variantRow" (click)="getDataFor(clinicalTrial)">
-              <td>{{clinicalTrial.nci_id}}</td>
-              <td>{{clinicalTrial.phase}}</td>
-              <td>{{clinicalTrial.brief_title}}</td>
-              <td>{{clinicalTrial.drugsToString()}}</td>
-              <td>{{clinicalTrial.principal_investigator}}</td>
-            </tr>
-            </tbody>
-          </table>
-        </ng-template>
-      </ngb-panel>
-    </ngb-accordion>
+    <table class="table table-hover table-bordered">
+      <thead>
+      <tr>
+        <th>Clinical Trial ID</th>
+        <th>Phase</th>
+        <th>Brief Title</th>
+        <th>Drugs</th>
+        <th>Principal Investigator</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr *ngFor="let clinicalTrial of clinicalTrials" class="variantRow" (click)="getDataFor(clinicalTrial)">
+        <td>{{clinicalTrial.nci_id}}</td>
+        <td>{{clinicalTrial.phase}}</td>
+        <td>{{clinicalTrial.brief_title}}</td>
+        <td>{{clinicalTrial.drugsToString()}}</td>
+        <td>{{clinicalTrial.principal_investigator}}</td>
+      </tr>
+      </tbody>
+    </table>
   `
 })
 export class ClinicalTrialsComponent implements OnInit {
   constructor (public clinicalTrialsService: ClinicalTrialsService) {}
 
   // One set of clinical trials per gene variant.
-  variants: Variant[];
-  clinicalTrials: ClinicalTrialReference[][] = [];
+  @Input() forVariant: Variant;
+  clinicalTrials: ClinicalTrialReference[] = [];
 
   ngOnInit(): void {
-    // Obtain a local copy of the variants for the service.
-    this.variants = USER_SELECTED_VARIANTS;
+    console.log('Called');
+    if (!this.forVariant) {
+      return;
+    }
 
     // Populate clinical trials.
-    for (let variantIndex = 0; variantIndex < this.variants.length; variantIndex++) {
-      this.clinicalTrials.push([]);
-      this.clinicalTrialsService.searchClinicalTrials(this.variants[variantIndex]).subscribe(trials => this.clinicalTrials[variantIndex] = trials);
-    }
+    this.clinicalTrialsService.searchClinicalTrials(this.forVariant).subscribe(trials => this.clinicalTrials = trials);
   }
 
   getDataFor(reference: ClinicalTrialReference) {
--- src/app/visualize-results/visualize-results.component.ts
+++ src/app/visualize-results/visualize-results.component.ts
@@ -2,26 +2,34 @@
  * Wrapper class of tabs for a bunch of post- data-entry components (Clinical Trials, etc.)
  */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Variant } from '../global/genomic-data';
+import { USER_SELECTED_VARIANTS } from '../data-entry/data-entry-form.component';
 
 @Component({
   selector: 'visualize-results',
   template: `
     <div class="root">
       <ngb-tabset [destroyOnHide]="false" >
-        <ngb-tab title="Relevant Clinical Trials">
+        <ngb-tab *ngFor="let variant of variants; let i = index;" title="{{variant.toIntelligentDisplayRepresentation()}}">
           <ng-template ngbTabContent>
-            <clinical-trials></clinical-trials>
-          </ng-template>
-        </ngb-tab>
-        <ngb-tab title="Drugs">
-          <ng-template ngbTabContent>
-            <p>Tab 2 Content</p>
+            <ngb-accordion #acc="ngbAccordion">
+              <ngb-panel title="Relevant Clinical Trials">
+                <ng-template ngbPanelContent>
+                  <clinical-trials [forVariant]="variant"></clinical-trials>
+                </ng-template>
+              </ngb-panel>
+            </ngb-accordion>
           </ng-template>
         </ngb-tab>
       </ngb-tabset>
     </div>
   `
 })
-export class VisualizeResultsComponent {
+export class VisualizeResultsComponent implements OnInit {
+  variants: Variant[];
+
+  ngOnInit() {
+    this.variants = USER_SELECTED_VARIANTS;
+  }
 }
