d695d2f607271cead9930fd60d9f81fe21e253a2
Makiah Bennett <makiahtbennett@gmail.com>
2017-08-11 22:07:34 -0400

    Remove OnInit from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -22,9 +22,6 @@ import { Component, OnInit } from "@angular/core";
   `]
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = "SMART EHR Module (Angular)";
-
-  ngOnInit(): void {
-  }
 }
