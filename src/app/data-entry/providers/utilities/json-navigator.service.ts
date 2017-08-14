010d9fe93c19cf5f4f70a29a3495098eaa34ba67
Makiah Bennett <makiahtbennett@gmail.com>
2017-08-13 23:20:43 -0400

    Set up the precursory framework.   for the JSON navigator.

---
+++ src/app/data-entry/providers/utilities/json-navigator.service.ts
@@ -0,0 +1,4 @@
+/**
+ * TODO: This class should eventually take over for the myvariant.info querier's JSON searching, since that service should be
+ * leveregable by all services which require it.
+ */
