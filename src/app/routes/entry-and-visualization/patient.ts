d22d180fc22152cb1b75779a55e0e2aa2a8c2cce
Varun Suraj <varunsuraj@gmail.com>
2020-06-14 10:43:22 -0400

    patient and condition information storage

--- src/app/routes/entry-and-visualization/patient.ts
+++ src/app/routes/entry-and-visualization/patient.ts
@@ -8,11 +8,40 @@ Contains two classes:
 
 // The Patient class described at the top of the file
 export class Patient {
-    name: string;
+    firstName: string;
+    lastName: string;
+    zipCode: string;
+    gender: string;
+    age: number;
+    conditions: Condition[] = [];
+    alreadyContainedCodes: string[] = [];
+
     constructor(
+        firstName: string,
+        lastName: string,
+        zipCode: string,
+        gender: string,
+        age: number
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.zipCode = zipCode;
+        this.gender = gender;
+        this.age = age;
+    }
+}
+
+
+export class Condition {
+    code: string;
+    name: string;
+    display: string;
+    constructor(
+        code: string,
         name: string
-    )
-    {
+    ) {
+        this.code = code;
         this.name = name;
+        this.display = code + " " + name;
     }
 }
