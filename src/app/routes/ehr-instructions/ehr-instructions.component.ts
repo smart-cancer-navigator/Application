b1f8354ebef8a50df2cac1a9433020261ad3ed0f
Varun Suraj <varunsuraj@gmail.com>
2020-06-14 23:20:06 -0400

    va login button activation

--- src/app/routes/ehr-instructions/ehr-instructions.component.ts
+++ src/app/routes/ehr-instructions/ehr-instructions.component.ts
@@ -1,17 +1,25 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 import { CMSService } from "../login-services/cms.service"
-import { ActivatedRoute } from "@angular/router";
+import { VAService } from "../login-services/va.service"
+
 
 @Component({templateUrl: 'ehr-instructions.component.html'})
 
 export class EHRInstructionsComponent {
     constructor(
-        private cmsService: CMSService
+        private cmsService: CMSService,
+        private vaService: VAService
     ) {}
 
     public cmsSignIn() {
         let auth:string = 
             `https://sandbox.bluebutton.cms.gov/v1/o/authorize/?client_id=${this.cmsService.clientId}&redirect_uri=http://localhost:4200/app&response_type=code&state=test1`;
+        location.href = auth;
+    }
+
+    public vaSignIn() {
+        var scope = encodeURIComponent("openid offline_access profile email launch/patient veteran_status.read patient/Observation.read patient/Patient.read patient/Condition.read");
+        var auth: string = `https://sandbox-api.va.gov/oauth2/authorization/?client_id=${this.vaService.clientId}&redirect_uri=http://localhost:4200/app&response_type=code&scope=${scope}&state=test1`;
         location.href = auth;
     }
 }
