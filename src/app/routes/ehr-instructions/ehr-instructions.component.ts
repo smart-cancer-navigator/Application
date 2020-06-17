46aa73a7fc1525757d0e779f77c547412aa76820
Varun Suraj <varunsuraj@gmail.com>
2020-06-16 23:21:11 -0400

    login component changes to create stage status

--- src/app/routes/ehr-instructions/ehr-instructions.component.ts
+++ src/app/routes/ehr-instructions/ehr-instructions.component.ts
@@ -11,13 +11,17 @@ export class EHRInstructionsComponent {
         private vaService: VAService
     ) {}
 
+    // called when the "Login to CMS" button is clicked.
     public cmsSignIn() {
+        localStorage.setItem("cmsUser", "attempt");
         let auth:string = 
             `https://sandbox.bluebutton.cms.gov/v1/o/authorize/?client_id=${this.cmsService.clientId}&redirect_uri=http://localhost:4200/app&response_type=code&state=test1`;
         location.href = auth;
     }
 
+    // called when the "Login to VA" button is clicked.
     public vaSignIn() {
+        localStorage.setItem("vaUser", "attempt");
         var scope = encodeURIComponent("openid offline_access profile email launch/patient veteran_status.read patient/Observation.read patient/Patient.read patient/Condition.read");
         var auth: string = `https://sandbox-api.va.gov/oauth2/authorization/?client_id=${this.vaService.clientId}&redirect_uri=http://localhost:4200/app&response_type=code&scope=${scope}&state=test1`;
         location.href = auth;
